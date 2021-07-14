b227c75755786c695f06ec953a11ee3733e36c8c
developer one <chideraamazu@gmail.com>
2021-07-14 08:46:49 +0200

    changes updated

---
+++ typescript2.js
@@ -0,0 +1,8 @@
+This is a sample text file, text is ok
+
+[D[D[D[Dls
+[A[A[A[A[A[[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[B[B[C[C[C[C[C[C[C[A[B
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[B[A[A[A[A[A[B[B[B[B[B
+
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
